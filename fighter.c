6734fe6114acca9e38d0df326fd19f7a29d4b667
KarlWithK <jocelinc60@outlook.com>
2020-10-28 14:27:02 -0400

    Fixed my free funciton

--- fighter.c
+++ fighter.c
@@ -26,4 +26,4 @@ void increase_deaths(struct fighter *street_name) {
   street_name->times_revieved++;
 }
 
-void clear_fighter(struct fighter *fighter_name) { free(fighter_name); }
+void clear_fighter(struct fighter *fighter_name) { free(fighter_name->name), free(fighter_name); }
