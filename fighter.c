68d452d0a285a46d5b8f2711fc0367113756adef
KarlWithK <jocelinc60@outlook.com>
2020-10-28 19:39:42 -0400

    testing

--- fighter.c
+++ fighter.c
@@ -5,21 +5,21 @@
 
 void print_fighter(struct fighter *street_name) {
   printf("Fighter's street name: %s, powerlevel: %d, times killed: %d\n",
-         street_name->name, street_name->powerlevel,
+         street_name->street_name, street_name->powerlevel,
          street_name->times_revieved);
 }
 
 struct fighter *create_fighter(char *street_name, int level, int deaths) {
   struct fighter *mars = malloc(sizeof(struct fighter));
   // Pointing to a char pointer so have to use this function below
-  mars->name = strdup(street_name);
+  mars->street_name= strdup(street_name);
   mars->powerlevel = level;
   mars->times_revieved = deaths;
   return mars;
 }
 
 void change_street_name(struct fighter *street_name, char *new_steet_name) {
-  street_name->name = strdup(new_steet_name);
+  street_name->street_name= strdup(new_steet_name);
 }
 
 void increase_deaths(struct fighter *street_name) {
@@ -27,7 +27,7 @@ void increase_deaths(struct fighter *street_name) {
 }
 
 void *clear_fighter(struct fighter *fighter_name) {
-  free(fighter_name->name);
+  free(fighter_name->street_name);
   free(fighter_name);
   return NULL;
 }
